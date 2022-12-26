07c78afe01bb5b86876b310f447351aa0fcac63a
singhtushar689 <singhtushar689@gmail.com>
2022-12-26 13:34:53 +0530

    Using Array

--- Program.cs
+++ Program.cs
@@ -2,11 +2,41 @@
 
 namespace Generic
 {
-    class Program
+    public class Program
     {
+        public static void FindMaximum(int[] inputArray)
+        {
+            foreach(int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+        public static void FindMaximum(double[] inputArray)
+        {
+            foreach(double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        public static void FindMaximum(char[] inputArray)
+        {
+            foreach(char element in inputArray) 
+            { 
+                Console.WriteLine(element); 
+            }
+        }
+
        public static void Main(string[] args) 
        {
             Console.WriteLine("Hello World!");
-       }
+            int[] intArray = { 3, 7, 2, 9, 8, 6, 1, 5, 4 };
+            char[] charArray = { 'a', 'b', 'd', 'e', 'f' };
+            double[] doubleArray = { 8.6, 9.7, 5.8, 2.6, 3.9 };
+
+            FindMaximum(intArray);
+            FindMaximum(charArray);
+            FindMaximum(doubleArray);
+        }
     }
 }
