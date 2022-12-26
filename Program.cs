1a181a2975ae0f7fdbbb398d3942ae5098c7bcba
singhtushar689 <singhtushar689@gmail.com>
2022-12-26 13:10:21 +0530

    Hello World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+
+namespace Generic
+{
+    class Program
+    {
+       public static void Main(string[] args) 
+       {
+            Console.WriteLine("Hello World!");
+       }
+    }
+}
