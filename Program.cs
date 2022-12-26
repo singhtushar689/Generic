05c9b6a3f80383454c93c663245471b2c8d9bf59
singhtushar689 <singhtushar689@gmail.com>
2022-12-26 20:05:45 +0530

    Finding maximum String

--- Program.cs
+++ Program.cs
@@ -8,12 +8,12 @@ namespace Generic
         public static void Main(string[] args)
         {
             FIndMax max= new FIndMax();
-            
-            float firstNumber = 5.6f;
-            float secondNumber = 7.7f;
-            float thirdNumber = 9.8f;
-            float result = max.FindMaxFloat(firstNumber, secondNumber, thirdNumber);
-            Console.WriteLine("The maximum float number among the three float number is {0}",result);
+
+            string first = "Tushar";
+            string second = "Sachin";
+            string third = "Rohan";
+            string result = max.FindMaxString(first, second, third);
+            Console.WriteLine("The maximum String among the three strings is {0}",result);
         }
     }
 }
