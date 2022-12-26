47013b7b7dc483d4744e64a8a3ebef64881046cb
singhtushar689 <singhtushar689@gmail.com>
2022-12-26 19:56:32 +0530

    Find Max Float Number

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@ namespace Generic
         {
             FIndMax max= new FIndMax();
             
-            int firstNumber = 56;
-            int secondNumber = 77;
-            int thirdNumber = 98;
-            int result = max.FindMaxInteger(firstNumber, secondNumber, thirdNumber);
-            Console.WriteLine(result);
+            float firstNumber = 5.6f;
+            float secondNumber = 7.7f;
+            float thirdNumber = 9.8f;
+            float result = max.FindMaxFloat(firstNumber, secondNumber, thirdNumber);
+            Console.WriteLine("The maximum float number among the three float number is {0}",result);
         }
     }
 }
